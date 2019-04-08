0376a444406b03d72c79c92bbbac084a47657ad4
steven-terrana <steventerrana@gmail.com>
2019-04-08 13:33:29 -0400

    Create pipeline_config.groovy

---
+++ pipeline_config.groovy
@@ -0,0 +1,9 @@
+/*
+  specify which libraries to load: 
+    In the Governance Tier configuration file, 
+    these should be configurations common across 
+    all apps governed by this config. 
+*/
+libraries{
+  sonarqube
+}
